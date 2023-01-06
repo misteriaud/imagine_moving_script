53c40d847ea1054de4bd09abd1ecc56118d7b998
Maxime Riaud <maximeriaud@Imac-Ass-Prod.local>
2023-01-06 14:11:22 +0100

    only change uid

--- moving_script.py
+++ moving_script.py
@@ -41,7 +41,7 @@ class Item:
     def move_to(self, new_path):
         try:
             new_path = shutil.move(self.path, new_path)
-            shutil.chown(new_path, user=self.uid, group="GRP_NAS_Tampon")
+            shutil.chown(new_path, user=self.uid)
             logging.debug(f'move {self.path} to {new_path}')
         except shutil.Error as e:
             logging.error(f'error: couldn\'t move {self.path} to {new_path} ({e})')
